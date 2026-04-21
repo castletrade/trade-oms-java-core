d1b6edd955409a549d52127852032f0d1c854be4
Cesar Castillo <castleaffiliates1@gmail.com>
2026-04-20 20:44:42 -0400

    test(java): implement spring context load test

---
+++ src/test/java/com/castletrade/oms/CastleTradeOmsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.castletrade.oms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastleTradeOmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+        // Basic audit test to ensure Spring Context is valid for CI/CD
+	}
+
+}
